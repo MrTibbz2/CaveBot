31c68af65c55dd1e193cfd8afa067579ecd08aba
ArchieBed <archieBeez@gmail.com>
2025-06-24 14:04:00 +1000

    yes

--- spikeprime/src/basicdemo.py
+++
@@ -1,9 +0,0 @@
-from primeCommands import Prime
-
-prime = Prime("NSE_Pybricks")  # Replace with your hub's name
-
-prime.moveForward(100)         # Move forward 100 cm
-prime.turnRight(90, 10)            # Turn right 90 degrees
-prime.moveBackwards(50)        # Move backwards 50 cm
-prime.stop(0, 0)               # Stop the motors
-prime.partyTime()              # Do a fun action
--- spikeprime/src/testwalk.py
+++
@@ -1,9 +0,0 @@
-import primeCommands
-
-prime = primeCommands.Prime("NSE_Pybricks")
-
-for i in range(4):
-    prime.moveForward(40)
-    prime.turnRight(90)
-
-prime.turnRight(90, 10)
