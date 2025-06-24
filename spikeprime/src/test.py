53dd8878946d3283bb33f4cf20107f830cae6278
ArchieBed <archieBeez@gmail.com>
2025-06-25 08:28:27 +1000

    cold

--- spikeprime/src/test.py
+++ spikeprime/src/test.py
@@ -3,6 +3,8 @@ import primeCommands
 prime = primeCommands.Prime("NSE_Pybricks")
 
 for i in range(4):
-    prime.moveForward(50)
+    prime.moveForward(40)
     prime.turnright()
+prime.partyTime()
+
 
