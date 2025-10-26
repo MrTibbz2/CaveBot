c026964b87c042c97c0e3ea35da780adb5fc1842
Lachlan Mckenna <155355235+MrTibbz2@users.noreply.github.com>
2025-10-26 15:31:14 +1100

    try running this archie

--- PrimeDrive/src/square.py
+++ PrimeDrive/src/square.py
@@ -1,18 +1,34 @@
 # Copyright (c) 2025 Archie Bradby
 # All rights reserved.
 
+import time
 import primeCommands
 
 hub_name = "NSE_Pybricks"
 
 prime = primeCommands.Prime(hub_name)
+def startscan():
+    print("Starting scan...")
 
-# prime.moveForward(24)
+def stopscan():
+    print("Stopping scan...")
+def Mapmove(yay):
+    print(f"Moving forward: {yay}")
+
+startscan()
+prime.moveForward(24)
+while prime.isMoving() == False:
+    time.sleep(0.1)
+last_distance = 0.0
+while prime.isMoving():
+    moved_distance = float(prime.getDistance()) - last_distance
+    Mapmove(moved_distance)
+    last_distance = float(prime.return_payload())
+stopscan()
+    
+
 # prime.turnLeft()
 # prime.moveForward(60)
 # prime.turnRight()
 # prime.moveForward(30)
-prime.moveForward(30)
 
-while prime.isMoving():
-    print(prime.return_payload())
