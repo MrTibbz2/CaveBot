995618be331d5fa87a51168bf5fbde219f6de224
Lachlan Mckenna <155355235+MrTibbz2@users.noreply.github.com>
2025-10-26 16:23:16 +1100

    updated maze impl

--- CaveCore/services/PrimeDrive/service.py
+++ CaveCore/services/PrimeDrive/service.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2025 Archie Bradby
 # All rights reserved.
 
+import time
 from ..service import Service
 from .primeCommands import Prime
 
@@ -46,3 +47,41 @@ class PrimeDriveService(Service):
     def turnRight(self):
         if self.prime:
             self.prime.turnRight()
+
+    def scan_and_move(self, distance, move_callback, start_scan, stop_scan):
+        if not self.prime:
+            return
+        
+        start_scan()
+        self.prime.moveForward(distance)
+        
+        start_wait_deadline = time.time() + 5.0
+        while not self.prime.isMoving():
+            if time.time() > start_wait_deadline:
+                print("Timeout waiting for movement to start")
+                break
+            time.sleep(0.05)
+        
+        last_distance = 0.0
+        total_distance = 0.0
+        
+        while self.prime.isMoving():
+            payload = self.prime.return_payload()
+            try:
+                current = float(payload)
+            except (TypeError, ValueError):
+                time.sleep(0.1)
+                continue
+            
+            moved_distance = current - last_distance
+            if moved_distance > 0:
+                move_callback(moved_distance)
+                total_distance += moved_distance
+                print(f"Total distance moved: {total_distance}")
+            
+            last_distance = current
+            time.sleep(0.1)
+        
+        stop_scan()
+
+    
